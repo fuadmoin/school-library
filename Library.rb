0f6b7bf75a05d833883ea65acfe8b18733944ae1
fuadmoin <fuadamoin@gmail.com>
2023-05-22 21:22:25 +0300

    Fix linter errors.

--- Library.rb
+++ Library.rb
@@ -1,54 +1,41 @@
 class Person
-  def initialize(age, name = "unknown", parent_permission = true)
+  def initialize(age, name = 'unknown', parent_permission: true)
     @name = name
     @age = age
     @parent_permission = parent_permission
     @id = 0
   end
 
-  def get_id
-    @id
-  end
+  attr_accessor :name, :age
 
-  def get_name
-    @name
-  end
+  attr_reader :id
 
-  def get_age
-    @age
-  end
-
-  def set_name(name)
-    @name = name
-  end
-
-  def set_age(age)
-    @age = age
-  end
-
-  private def of_age
-    @age >= 18
-  end
-
-  public def can_use_services
+  def can_use_services
     of_age || @parent_permission
+  end
+
+  private
+
+  def of_age
+    @age >= 18
   end
 end
 
 class Student < Person
-  def initialize(age, classroom, parent_permission = true, name = "unknown")
-    super(age, name, parent_permission)
+  def initialize(age, classroom, name = 'unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
+    @hooky = '¯\(ツ)/¯'
   end
 
   def play_hooky
-    "¯\(ツ)/¯"
+    @hooky
   end
 end
 
 class Teacher < Person
-  def initialize(age, specialization, name = "unknown", parent_permission = true)
-    super(age, name, parent_permission)
+  def initialize(age, specialization, name = 'unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
@@ -59,10 +46,10 @@ end
 
 
 
-
-fuad = Student.new(17, "Grade 8", false, "Fuad")
-fuad.set_age(16)
+fuad = Student.new(17, 'Grade 8', 'Fuad', parent_permission: true)
+fuad.age = 16
+puts fuad.age
 puts fuad.can_use_services
 
-reviewer = Teacher.new(29, "Computer Science", "Jackson")
+reviewer = Teacher.new(29, 'Computer Science', parent_permission: false)
 puts reviewer.can_use_services
