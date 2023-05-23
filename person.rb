b5596c06c97969a9c6a75eaaa8aae39c92bfe7c1
fuadmoin <fuadamoin@gmail.com>
2023-05-23 18:39:41 +0300

    Decorate a class.

--- person.rb
+++ person.rb
@@ -1,4 +1,6 @@
-class Person
+require_relative './nameable'
+require_relative './name_decorator'
+class Person < Nameable
   attr_accessor :name, :age
 
   attr_reader :id
@@ -8,10 +10,15 @@ class Person
     @age = age
     @parent_permission = parent_permission
     @id = 0
+    super()
   end
 
   def can_use_services
     of_age || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   private
@@ -20,3 +27,11 @@ class Person
     @age >= 18
   end
 end
+
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
